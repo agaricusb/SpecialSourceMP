341a32a4229d8e38dcd790e4093ff10e1846c787
Agaricus <agaricusb@yahoo.com>
2013-03-03 16:27:48 -0800

    Add support for remapped dependencies, for inheritance lookup
    Required for correctly remapping methods/fields defined in
    superclasses but accessed on subclass instances, etc. Normally
    you would add a <remappedDependency><param>... in the 'remap'
    goal for each artifact remapped with 'install-remapped-file'.

--- src/main/java/net/md_5/specialsource/mavenplugin/RemapMojo.java
+++ src/main/java/net/md_5/specialsource/mavenplugin/RemapMojo.java
@@ -1,11 +1,14 @@
 package net.md_5.specialsource.mavenplugin;
 
-import net.md_5.specialsource.Jar;
-import net.md_5.specialsource.JarMapping;
-import net.md_5.specialsource.JarRemapper;
+import net.md_5.specialsource.*;
 import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.ArtifactResolutionRequest;
+import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -18,6 +21,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Create a remapped version of the main project artifact
@@ -36,7 +40,19 @@ public class RemapMojo extends AbstractMojo {
     @Component
     private MavenProjectHelper projectHelper;
 
-    /**
+    @Component
+    private ArtifactResolver artifactResolver;
+
+    @Component
+    private ArtifactFactory artifactFactory;
+
+    @Parameter( property = "localRepository", required = true, readonly = true )
+    private ArtifactRepository localRepository;
+
+    @Parameter( defaultValue = "${project.remoteArtifactRepositories}" )
+    private List<ArtifactRepository> remoteRepositories;
+
+   /**
      * The destination directory for the shaded artifact.
      */
     @Parameter( defaultValue = "${project.build.directory}" )
@@ -78,8 +94,10 @@ public class RemapMojo extends AbstractMojo {
     private String inShadeRelocation;
     @Parameter
     private String outShadeRelocation;
+    @Parameter
+    private String[] remappedDependencies;
 
-    public void execute() throws MojoExecutionException {
+    public void execute() throws MojoExecutionException, MojoFailureException {
         if (project.getArtifact().getFile() == null || !project.getArtifact().getFile().isFile()) {
             // message borrowed from maven-shade-plugin
             getLog().error( "The project main artifact does not exist. This could have the following" );
@@ -96,25 +114,45 @@ public class RemapMojo extends AbstractMojo {
                     "Failed to create remapped artifact, " + "project main artifact does not exist." );
         }
 
-        File inputJar = project.getArtifact().getFile();
-        File outputJar = remappedArtifactFileWithClassifier();
+        File inputFile = project.getArtifact().getFile();
+        File outputFile = remappedArtifactFileWithClassifier();
 
         try {
+            // Load mappings
             JarMapping mapping = new JarMapping();
-
             mapping.loadMappings(srgIn, reverse, numeric, inShadeRelocation, outShadeRelocation);
 
+            Jar inputJar = Jar.init(inputFile);
+
+            // Load remapped dependencies for inheritance lookup
+            InheritanceProviders inheritanceProviders = new InheritanceProviders();
+            for (String artifactString : remappedDependencies) {
+                String[] array = artifactString.split(":");
+                if (array.length != 4) {
+                    throw new MojoExecutionException("Invalid remapped dependency name, must be groupId:artifactId:version:type:classifier "+artifactString+" in "+array.length);
+                }
+                String groupId = array[0], artifactId = array[1], version = array[2], type = array[3], classifier = null;
+                Artifact artifact = artifactFactory.createArtifactWithClassifier(groupId, artifactId, version, type, classifier);
+                artifactResolver.resolve(artifact, remoteRepositories, localRepository);
+                File dependencyFile = artifact.getFile();
+                System.out.println("Adding inheritance "+dependencyFile.getPath());
+                inheritanceProviders.add(new JarInheritanceProvider(Jar.init(dependencyFile)));
+            }
+            inheritanceProviders.add(new JarInheritanceProvider(inputJar));
+            mapping.setFallbackInheritanceProvider(inheritanceProviders);
+
+            // Do the remap
             JarRemapper remapper = new JarRemapper(mapping);
-            remapper.remapJar(Jar.init(inputJar), outputJar);
+            remapper.remapJar(inputJar, outputFile);
 
             if (remappedArtifactAttached) {
                 getLog().info("Attaching remapped artifact.");
                 projectHelper.attachArtifact( project, project.getArtifact().getType(), remappedClassifierName,
-                        outputJar );
+                        outputFile);
             } else {
                 getLog().info("Replacing original artifact with remapped artifact.");
                 File originalArtifact = project.getArtifact().getFile();
-                replaceFile( originalArtifact, outputJar );
+                replaceFile( originalArtifact, outputFile);
             }
         } catch (Exception ex) {
             ex.printStackTrace();
